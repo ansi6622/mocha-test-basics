5fb4c0a09bb7b4ef46c297c284082e193e70874a
Anthony Simpson <ansi6622@colorado.edu>
2016-02-12 09:26:56 -0700

    commiting tests and test messages and anything else left behind

--- test/test.js
+++ test/test.js
@@ -1,7 +1,9 @@
 var code = require('../main.js')
 var expect = require('chai').expect
-describe("helloWorld", function() {
-  it('will greet us', function (){
-    expect(code.helloWorld()).to.equal("Hello World!")
+describe("tug_o_war", function(teams) {
+  it("will tug two arrays that are inside of an array: expect code to return string [[1,2,3,4,5], [5,4,3,2,1]] It's a tie! [[7,2,3,4,6], [5,4,3,2,1]] Team 1 wins! [[5,2,3,4,6], [5,4,3,2,8]] Team 2 wins!", function (){
+    expect(code.tug_o_war([[1,2,3,4,5], [5,4,3,2,1]])).to.equal("It's a tie!")
+    expect(code.tug_o_war([[7,2,3,4,6], [5,4,3,2,1]])).to.equal("Team 1 wins!")
+    expect(code.tug_o_war([[5,2,3,4,6], [5,4,3,2,8]])).to.equal("Team 2 wins!")
   })
 })
